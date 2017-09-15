5b604b410ef728554bc6a89044b537a051b9b0b3
tranki1 <kimhieutran100@gmail.com>
2017-09-15 12:38:17 +0300

    [BE] get profile by user ID

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -33,4 +33,25 @@ router.get(
   }
 );
 
+// @route GET api/profiles/user/:user_id
+// @desc Get profile by user_id
+// @access Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.userid = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: 'There is no profile for this user' })
+    );
+});
+
 module.exports = router;
