cd7f7c3d28ee22832b1e50b191885379a4c92734
tranki1 <kimhieutran100@gmail.com>
2017-09-18 15:09:34 +0300

    [BE] add question module

---
+++ models/Question.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//Create Schema
+const QuestionSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  ageandstage: {
+    type: String
+  },
+  topics: [{ name: String }],
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectID,
+        ref: 'users'
+      }
+    }
+  ],
+  answers: [
+    {
+      user: {
+        type: Schema.Types.ObjectID,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      username: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = Question = mongoose.model('question', QuestionSchema);
